86f5da6810d3759885c9c7d94c5d1b982c6fe991
unknown <diegovillagrasa@gmail.com>
2020-04-09 11:49:25 +0200

    Created the question service - Fixes #11

---
+++ src/app/services/question.service.spec.ts
@@ -0,0 +1,33 @@
+import { TestBed } from '@angular/core/testing';
+
+import { QuestionService } from './question.service';
+import { NotificationService } from './notification.service'
+import { ConnectionService } from './connection.service';
+import { LocalNotifications } from '@ionic-native/local-notifications/ngx';
+import { UniqueDeviceID } from '@ionic-native/unique-device-id/ngx';
+import { ConfigService } from './config.service';
+
+const testConfig: ConfigService = new ConfigService();
+
+describe('QuestionService', () => {
+  beforeEach(async () => {
+    
+    TestBed.configureTestingModule({
+      providers: [NotificationService, LocalNotifications, UniqueDeviceID, ConnectionService]
+    })
+    
+});
+
+  it('should be created', () => {
+    const service: QuestionService = TestBed.get(QuestionService);
+    expect(service).toBeTruthy();
+  });
+
+  /*it('should get the questions in a debate', async () => {
+    const qs: QuestionService = TestBed.get(QuestionService);
+    const cs: ConnectionService = TestBed.get(ConnectionService);
+    await cs.join(testConfig.getTestConfig()['TEST_DEBATE_CODE']);
+    var questions = await qs.getQuestions();
+    expect(questions).toBeDefined();
+  });*/
+});
---
+++ src/app/services/question.service.ts
@@ -0,0 +1,125 @@
+import { Injectable } from '@angular/core';
+import { NotificationService } from './notification.service'
+import { ConnectionService } from './connection.service';
+
+const NEW_QUESTION_TITLE: string = "Une nouvelle question est disponible";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class QuestionService {
+
+  //We need to save the question between pages
+  savedQuestion: any;
+
+  constructor(
+    private connection: ConnectionService,
+    private notification: NotificationService
+  ) { }
+  
+  /**
+   * Retrieves the questions of the debate
+   */    
+  public getQuestions() {
+    var that = this;
+    return new Promise(async function (resolve, reject) {
+      console.log("SOCKET", that.connection.isConnected())
+      that.connection.socket.emit("getQuestions",
+        {},
+        (questions: any[]) => {
+          resolve(questions);
+        }
+      );
+    });
+  }
+
+  /**
+   * Send's the user's answer of a closed question
+   * @param questionId  Id of the question
+   * @param answerId    Id of the answer
+   */
+  public answerQuestion(questionId: number, answerId: number) {
+    var that = this;
+    return new Promise(async function (resolve, reject) {
+      that.connection.socket.emit("answerQuestion",
+        {
+          questionId: questionId,
+          answerId: answerId
+        },
+        (result: boolean) => {
+          resolve(result);
+        }
+      );
+    });
+  }
+
+  /**
+   * Send's the user's answer of an open question
+   * @param questionId  Id of the question
+   * @param answer      User's answer
+   */
+  public answerOpenQuestion(questionId: number, answer: string) {
+    var that = this;
+    return new Promise(async function (resolve, reject) {
+      that.connection.socket.emit("answerOpenQuestion",
+        {
+          questionId: questionId,
+          answer: answer
+        },
+        (result: boolean) => {
+          resolve(result);
+        }
+      );
+    });
+  }
+
+  /**
+   * Calls a function when a new question is available
+   * @param callback Function to call
+   */
+  public onNewQuestion(callback: Function) {
+    this.connection.socket.on("newQuestion",
+      (question: any) => {
+        // Send a notification
+        this.notification.displayNotification(
+          NEW_QUESTION_TITLE,
+          question['title']
+        )
+
+        // Call the callback
+        callback(question)
+      }
+    );
+  }
+
+  /**
+   * Suggest a question to be added to the debate
+   * @param question Suggested question
+   */
+  public suggestQuestion(question: any) {
+    var that = this;
+    return new Promise(async function (resolve, reject) {
+      that.connection.socket.emit("suggestQuestion",
+        question, (result: boolean) => {
+          resolve(result);
+        }
+      );
+    });
+  }
+
+  /**
+   * Saves the question for use in another page
+   * @param question Question to save
+   */
+  public saveQuestion(question: any) {
+    this.savedQuestion = question;
+  }
+
+  /**
+   * Retrieves the saved question
+   */
+  public getSavedQuestion() {
+    return this.savedQuestion;
+  }
+
+}
