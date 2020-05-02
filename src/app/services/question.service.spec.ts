190cce3a465f4b22a325c2819b5761bc02f69065
unknown <diegovillagrasa@gmail.com>
2020-05-02 17:29:11 +0200

    Updated question service to use the new config

--- src/app/services/question.service.spec.ts
+++ src/app/services/question.service.spec.ts
@@ -5,9 +5,7 @@ import { NotificationService } from './notification.service'
 import { ConnectionService } from './connection.service';
 import { LocalNotifications } from '@ionic-native/local-notifications/ngx';
 import { UniqueDeviceID } from '@ionic-native/unique-device-id/ngx';
-import { ConfigService } from './config.service';
 
-const testConfig: ConfigService = new ConfigService();
 
 describe('QuestionService', () => {
   beforeEach(async () => {
@@ -26,6 +24,7 @@ describe('QuestionService', () => {
   /*it('should get the questions in a debate', async () => {
     const qs: QuestionService = TestBed.get(QuestionService);
     const cs: ConnectionService = TestBed.get(ConnectionService);
+    const testConfig: ConfigService = TestBed.get(ConfigService);
     await cs.join(testConfig.getTestConfig()['TEST_DEBATE_CODE']);
     var questions = await qs.getQuestions();
     expect(questions).toBeDefined();
