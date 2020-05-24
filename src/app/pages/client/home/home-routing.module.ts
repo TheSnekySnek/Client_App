4a5acbe98b01b04a45fc572b4cda392f22f73f63
Robin Müller <33422394+AziXus@users.noreply.github.com>
2020-05-24 23:00:00 +0200

    Added new home page with tabs

---
+++ src/app/pages/client/home/home-routing.module.ts
@@ -0,0 +1,49 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { HomePage } from './home.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomePage,
+    children: [
+      {
+        path: 'questions',
+        children: [
+          {
+            path: '',
+            loadChildren: () =>
+                import('../questions/questions.module').then(m => m.QuestionsPageModule)
+          }
+        ]
+      },
+      // {
+      //   path: 'suggestions',
+      //   children: [
+      //     {
+      //       path: '',
+      //       loadChildren: () =>
+      //           import('../suggestions/suggestions.module').then(m => m.SuggestionsPageModule)
+      //     }
+      //   ]
+      // },
+      {
+        path: '',
+        redirectTo: '/home/questions',
+        pathMatch: 'full'
+      }
+    ]
+  },
+  {
+    path: '',
+    redirectTo: '/home/questions',
+    pathMatch: 'full'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class HomePageRoutingModule {}
