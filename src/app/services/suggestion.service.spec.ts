c2c7c9011bc7e476609e4e1d849d011c934d595a
Robin Müller <33422394+AziXus@users.noreply.github.com>
2020-05-25 11:15:46 +0200

    Fixed suggestion service crash by including providers

--- src/app/services/suggestion.service.spec.ts
+++ src/app/services/suggestion.service.spec.ts
@@ -1,9 +1,19 @@
 import { TestBed } from '@angular/core/testing';
 
 import { SuggestionService } from './suggestion.service';
+import { QuestionService } from './question.service';
+import { NotificationService } from './notification.service'
+import { ConnectionService } from './connection.service';
+import { LocalNotifications } from '@ionic-native/local-notifications/ngx';
+import { UniqueDeviceID } from '@ionic-native/unique-device-id/ngx';
 
 describe('SuggestionService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [NotificationService, LocalNotifications, UniqueDeviceID, ConnectionService]
+    })
+
+  });
 
   it('should be created', () => {
     const service: SuggestionService = TestBed.get(SuggestionService);
