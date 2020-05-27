9d5bd1cb395725c2edd84f99730e39e13e18f37b
Naludrag <stephanecarvalho81@gmail.com>
2020-05-27 15:19:18 +0200

    Chnaged host to sondage.codeheure.com

--- src/app/services/config.service.ts
+++ src/app/services/config.service.ts
@@ -2,17 +2,17 @@ import { Injectable } from '@angular/core';
 
 
 const GLOBAL_CONFIG = {
-  "environment": "PROD",
+  "environment": "DEV",
   "constants": {
       "prod": {
           "admin": {
-              "SOCKET_ADDRESS"        : "127.0.0.1",
+              "SOCKET_ADDRESS"        : "sondage.codeheure.com",
               "SOCKET_PORT"           : 8080,
               "SOCKET_PATH"           : "/socket-io",
               "SOCKET_NAMESPACE"      : "/admin"
           },
           "client": {
-              "SOCKET_ADDRESS"        : "127.0.0.1",
+              "SOCKET_ADDRESS"        : "sondage.codeheure.com",
               "SOCKET_PORT"           : 8080,
               "SOCKET_PATH"           : "/socket-io",
               "SOCKET_NAMESPACE"      : "/DEBATE-"
@@ -20,13 +20,13 @@ const GLOBAL_CONFIG = {
       },
       "dev": {
           "admin": {
-              "SOCKET_ADDRESS"        : "127.0.0.1",
+              "SOCKET_ADDRESS"        : "sondage.codeheure.com",
               "SOCKET_PORT"           : 4356,
               "SOCKET_PATH"           : "/socket-io",
               "SOCKET_NAMESPACE"      : "/admin"
           },
           "client": {
-              "SOCKET_ADDRESS"        : "127.0.0.1",
+              "SOCKET_ADDRESS"        : "sondage.codeheure.com",
               "SOCKET_PORT"           : 4356,
               "SOCKET_PATH"           : "/socket-io",
               "SOCKET_NAMESPACE"      : "/DEBATE-"
