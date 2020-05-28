6fe6c0d8a836f1461d3cf19736bfcfe3daa8b420
Robin Müller <33422394+AziXus@users.noreply.github.com>
2020-05-28 15:17:47 +0200

    Fixed travis build

--- src/app/pages/admin/admin-begin/admin-begin.page.spec.ts
+++ src/app/pages/admin/admin-begin/admin-begin.page.spec.ts
@@ -2,6 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
 import { AdminBeginPage } from './admin-begin.page';
+import {RouterTestingModule} from "@angular/router/testing"
 
 describe('AdminBeginPage', () => {
   let component: AdminBeginPage;
@@ -10,7 +11,7 @@ describe('AdminBeginPage', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ AdminBeginPage ],
-      imports: [IonicModule.forRoot()]
+      imports: [IonicModule.forRoot(), RouterTestingModule]
     }).compileComponents();
 
     fixture = TestBed.createComponent(AdminBeginPage);
